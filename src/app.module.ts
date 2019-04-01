38489e737656727d82e25639439bf6824cf9ca67
Leonardo Ribeiro <contato@leoribeiroweb.com.br>
2019-04-01 19:43:39 -0300

    mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,11 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-const mongoHost = process.env.MONGO_HOST || 'localhost';
-const mongoPort = process.env.MONGO_PORT || '27017';
-const mongoDatabase = process.env.MONGO_DATABASE || 'simple-api';
-const options: any = {
-  useNewUrlParser: true,
-  createIndexes: true,
-};
-
-if (process.env.NODE_ENV === 'production') {
-  options.auth = {
-    user: process.env.MONGO_USER,
-    pass: process.env.MONGO_PASS,
-  };
-}
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/simple-api';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://${mongoHost}:${mongoPort}/${mongoDatabase}`, options),
+    MongooseModule.forRoot(mongoURI, { useNewUrlParser: true }),
     UsersModule,
     AuthModule,
   ],
